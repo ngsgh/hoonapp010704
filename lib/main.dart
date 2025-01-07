116606b5b429bf35cd76cad57c8b1a66acea74f7
ngsgh <noognas00@gmail.com>
2025-01-07 17:06:08 +0900

    변경사항 업데이트

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page1224522'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page12245222'),
     );
   }
 }
