7e09a5e13c2d39737f0cb3f38f99c3879c4769f4
ngsgh <noognas00@gmail.com>
2025-01-07 17:03:09 +0900

    6

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page1224'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page12245'),
     );
   }
 }
